0f79a470b7a1bd734fca6e84c363d582d6ceecf8
ScalWeb Info <info@scalweb.com>
2019-10-26 17:33:12 +0200

    removing extra testing file

--- test.js
+++
@@ -1,8 +0,0 @@
-const {VM} = require('vm2');
-let result = new VM({
-    eval: false,
-    wasm: false,
-    timeout: 1000
-}).run('var x = Math.round(100.7); x;');
-console.log(result);
-// console.log('Never gets executed.');
